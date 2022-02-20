018083e4f12f544b4d5bdaf2de918f7cc456ef89
WinstonNguyen <winstonnguyen2317@gmail.com>
2022-02-20 01:54:09 -0600

    even numbers

---
+++ While Loop/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace SIX
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 1001; i++)
+            {
+                {
+                    if (i % 2 == 1)
+                        continue;
+
+                    else if (i % 6 == 0 && i % 9 == 0)
+                    {
+                        Console.WriteLine("SIXNINE");
+                    }
+                    else if (i % 6 == 0)
+                    {
+                        Console.WriteLine("SIX");
+                    }
+                    else if (i % 9 == 0)
+                    {
+                        Console.WriteLine("NINE");
+                    }
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+                   
